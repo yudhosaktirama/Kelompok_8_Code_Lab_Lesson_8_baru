71bfcab04be14be37af1d2a99a88bf7f317c22f4
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-26 14:02:55 +0700

    Menambahkan getter pada GameViewModel.kt agar properti atau nilai variabel yang private bisa diakses oleh fragment atau class lain

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,8 +4,15 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     private var _score = 0
-    private var _currentworldcount = 0
+    val score : Int
+        get() = _score
+    private var _currentWordCount = 0
+    val currentWordCount : Int
+        get() = _currentWordCount
     private var _currenScrambleWord= "test"
+    val currentScrambleWord: String
+        get() = _currenScrambleWord
+
 
 
 }

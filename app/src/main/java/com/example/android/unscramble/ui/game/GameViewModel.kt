238cf9a969a3d389adce8df22bb802d19b992752
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-26 13:59:25 +0700

    Memindahkan Properti Variabel ke dalam GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,5 +3,9 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    private var _score = 0
+    private var _currentworldcount = 0
+    private var _currenScrambleWord= "test"
+
 
 }

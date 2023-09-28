31748ead4cbd2c0f5e72e68b8a815123d145f38a
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-28 10:32:58 +0700

    akses  nilai score menggunakan value pada score

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -112,7 +112,7 @@ class GameFragment : Fragment() {
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored, gameViewModel.score))
+            .setMessage(getString(R.string.you_scored, gameViewModel.score.value))
             .setCancelable(false)
             .setNegativeButton(getString(R.string.exit)) { _, _ ->
                 exitGame()

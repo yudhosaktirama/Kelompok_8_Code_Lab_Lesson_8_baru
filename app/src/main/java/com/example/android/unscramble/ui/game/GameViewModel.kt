e2dc2f7165ac8cc49d579899b6d49ad5513dfbc3
yudhosaktirama <yudhosakti89@gmail.com>
2023-09-28 10:09:19 +0700

    Mengubah itial value _score dan _currentWordCount menjadi MutableLiveData serta mengubah getter yang bersangkutan menjadi LiveData

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,11 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private var _score = 0
-    val score : Int
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
         get() = _score
-    private var _currentWordCount = 0
-    val currentWordCount : Int
+    private var _currentWordCount = MutableLiveData<Int>()
+    val currentWordCount : LiveData<Int>
         get() = _currentWordCount
     private var _currenScrambleWord = MutableLiveData<String>()
     val currentScrambleWord: LiveData<String>
